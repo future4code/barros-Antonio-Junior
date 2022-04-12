9830723f793b882f80d475ff835378cf4e74739f
Antonio <antoniojunior_95@hotmail.com>
2022-04-12 11:56:52 -0300

    exercicio-de-variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,70 @@
+/* Exercicio 1: 
+1- let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+R= No primeiro ''console.log'' vai aparecer o resultado da ''b''
+que é 10, e no segundo ''console.log'', vai aparecer a mudança
+do ''b'' de 10, para 5.
+
+2- let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+R= Aparece o resultado tudo 10, por que todas as variaveis tem
+o valor de referencia da variavel ''a''.
+
+3- let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+R= Ele apaece uma caxinha com as informações acima.
+E faz o cauculo de quanto recebe de dinheiro a hora.*/
+
+// Exercicio 2
+
+// 1-
+
+let nome ="Maria"
+let idade = 20
+let oi ="Olá"
+let você ="você tem"
+let anos="anos"
+console.log( oi, nome , você, idade, anos )
+/* Resposta letra d: undefied , undefied. Não tinha valor atribuido.
+Resposta letra f: foi definido as duas variaveis string, e number, pois
+foi atribuidos valores de texto, e numero.*/
+
+// 2-
+
+prompt = 'Você está usando roupa azul?'- Sim
+prompt = "Você tem cabelos longos?"- Não
+prompt = "Você é uma pessoa alta?"- Não
+
+// 3 -
+
+let a = 10
+let b = 25
+console.log(a,b)
+
+a = 20
+b = 30
+console.log(a,b)
+
+
+
+
+
+
+
+
+
+
