a5114e08e173bb63f5ead1c26e4a285137de2fab
Antonio <antoniojunior_95@hotmail.com>
2022-04-19 19:02:35 -0300

    Exercicio-Strings-Array-Feito

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,69 @@
+/*Exercícios de interpretação de código 
+
+Exercicio 1 - 
+
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)
+
+R= Let Resultado1 = false
+
+R= let resultado2 = false
+
+R= let resultado3 = true
+
+R= let resultado4 = bolean*/
+
+/*Exercicio 2 -
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+
+R= Da o resultado da junção dos dois numeros escrito no prompt pelo usuario.
+Por exemplo: 1 Prompt = 10 , 2 Prompt = 20 , Resultado = 1020.
+
+Execicio 3-
+
+   let primeiroNumero = Number(prompt('Digite um numero'))
+   let segundoNumero = Number(prompt('Digite outro numero'))
+   let soma = primeiroNumero + segundoNumero
+   console.log(soma)*/
+
+/* Exercícios de escrita de código
+
+Exercicio 1 -*/
+
+let idadeMinha = 26
+let idadeAmigo= 25
+console.log("Sua idade é maior do que a do seu melhor amigo?", 
+idadeMinha > idadeAmigo) // R= true
+
+//Exercicio 2 -
+
+/* letra a */ let primeiroNumero = prompt (Number ("Coloque um numero par"))
+/* letra b*/  console.log(primeiroNumero % 2)
+/* letra c*/  console.log(primeiroNumero % 4) //Todos irão da numero exatos exceto quando não for numero inteiros.
+/* letra d*/  let segundoNumero = prompt (Number ("Coloque um numero impar"))
+              console.log(segundoNumero % 4) // Geralmente dara resultado impar
+// Exercicio 3 -
+
+let suaIdade1 = prompt(Number("Qual a sua idade?"))
+/* letra a */ console.log(suaIdade1 * 12)
+/* letra b*/ console.log(suaIdade1 * 365)
+/* letra c*/ console.log(suaIdade1 * 8760)
+
---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,60 @@
+// Exercicio de interpretação de codigo
+
+/* 1-a) let array
+console.log('a. ', array)
+R= a.
+
+b) array = null
+console.log('b. ', array)
+R= b. null
+
+c) array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+//R= c. 11
+
+let i = 0
+console.log('d. ', array[i])
+//R= d. 3
+array[i+1] = 19
+console.log('e. ', array)
+//R= 11
+
+const valor = array[i+6]
+console.log('f. ', valor)
+// R= 9
+
+// Exercicio 2
+
+/*const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+R= 27*/
+
+// Exercícios de escrita de código
+
+/* 1 = 
+const emaillDoUsuario = prompt("Qual é o seu email?")
+const nomeDoUsuario = prompt("Diga o seu nome !")
+console.log("O e-mail"+emaillDoUsuario+ "foi cadastrado com sucesso. Seja bem-vinda(o)," +nomeDoUsuario)*/
+
+/* 2=
+let array = `Lasanha
+Maçã
+Uva
+Macarrão
+Bolo`
+//a + b)
+console.log(array) 
+
+//c)
+const comidaPreferida = prompt("Qual a sua comida preferida?")
+console.log(array.replaceAll("Maçã",comidaPreferida))*/
+
+/* 3 =
+
+let listaDeTarefas =Array
+let tarefasDoUsuario = prompt("Quais a 3 tarefas que realizara hoje?")
+console.log(tarefasDoUsuario)
+let tarefasDoUsuarioFeita = prompt("Em numeração qual das tarefas acima voce fez primeiro?")
+console.log(tarefasDoUsuarioFeita.splice)*/
+
