9e46dc52974279bec1fe361de160e0a28685a28c
Antonio <antoniojunior_95@hotmail.com>
2022-05-03 22:15:52 -0300

    exercicio-lacos-feito

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,48 @@
+// Exercicio de interpretação de codigo
+// Questão 1 -
+/*let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)*/
+// R= Ele verifica quantos numeros pode ser menor que 5, resultado 10.
+
+// Questão 2
+/* const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}*/
+
+// R= a) Todos os intens da array.
+//    b) Não
+
+// 3) 
+/*const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "*"
+  }
+  console.log(linha)
+  quantidadeAtual++
+}*/
+// R= Vai resultar a quantidade em asteristicos.
+
+// Exercícios de escrita de código
+
+// Questão 1- 
+
+/*let respostaDoUsuario=(prompt("Quantos animais de estimação você tem?"))
+let numero = Number(respostaDoUsuario)
+
+if(numero = 0){
+console.log("Que pena! Você pode adotar um pet!")   
+} while (numero > 0){
+let respostaDoUsuario2 = prompt("Diga os nomes do seus bichos !")}
+console.log(respostaDoUsuario)*/
+
+
+// Questão 2
