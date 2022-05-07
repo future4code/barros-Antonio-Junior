b31a276683013494166c5398449135e1da5cefe1
Antonio <antoniojunior_95@hotmail.com>
2022-05-06 22:46:04 -0300

    projeot-blackjack-feito

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,32 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+    /*if(confirm("Quer iniciar uma rodada, sim ou não?")) {
+    let respostaUsuario = "sim"
+    console.log(respostaUsuario === "Vamos la")
+    Array= Number("A","2","3","4","5","6","7","8","9","10","J","Q","K")
+    const naipes = ["♦️", "♥️", "♣️", "♠️"]
+    const player1 = respostaUsuario
+ 
+   } else {
+      respostaUsuario = "não"
+      console.log(respostaUsuario === "O jogo acabou!")
+   }
+   const carta = comprarCarta(); {
+   cartaSorteada = "K"
+
+   }
+   console.log(carta.texto) 
+  carta.texto = cartaSorteada
+
+console.log(carta.valor) 
+carta.valor = cartaSorteada*/
