3128fee61be82d8668700865d1bf6fc6d3f8679e
Antonio <antoniojunior_95@hotmail.com>
2022-07-07 22:29:40 -0300

    exercicio-lista-2-feito

---
+++ modulo3/insta4/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Post from './components/Post/Post';
+import Post1 from './components/Post/post1'
+import './style.css'
+
+
+function App() {
+return(
+  <div className='MainContainer'>
+           <Post
+            nomeUsuario={'paulinha'}
+            fotoUsuario={'https://picsum.photos/50/50'}
+            fotoPost={'https://picsum.photos/200/150'}
+          />
+          <Post1>
+          nomeUsuario={'Marcos'}
+            fotoUsuario={'https://picsum.photos/50/50'}
+            fotoPost={'https://picsum.photos/200/150'}
+          </Post1>
+  </div>  
+  )
+}
+
+
+export default App;
