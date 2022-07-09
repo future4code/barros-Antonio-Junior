89d93569a7e312b227601f0477ccc034e50105d5
Antonio <antoniojunior_95@hotmail.com>
2022-07-08 23:03:36 -0300

    projeto-whatslab-feito

---
+++ modulo3/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,36 @@
+/*import styled from "styled-components";
+import {Cabecalho,Rodape,Principal} from "./styled"
+
+
+function App() {
+  return (
+    <div className="App">
+  <Cabecalho>
+    <p>LabeZap</p>
+  </Cabecalho>
+<Principal>
+<form>
+   <label>
+   <input type ="text"
+   name="name" />
+   </label>
+   <input type="subimit"
+   value="Enviar"/>
+
+   </form>
+   </Principal>
+
+<Rodape>
+  <p>Antonio Junior Copryghts 2022 Labenu Coporations</p>
+</Rodape>
+  
+
+
+</div>
+
+
+  )
+  ;
+}
+
+export default App;*/
