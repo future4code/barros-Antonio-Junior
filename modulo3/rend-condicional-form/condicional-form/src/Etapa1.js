f8d28c75846e11cdffcfeaab86ccf65e461a20f5
Antonio <antoniojunior_95@hotmail.com>
2022-07-12 22:46:29 -0300

    exercicio-rend-condicional-form-feito

---
+++ modulo3/rend-condicional-form/condicional-form/src/Etapa1.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function Etapa1() {
+  return (
+    <div className="Etapa1">
+      
+      <h1>Dados Gerais</h1>
+
+     <form name='f_cadastro' action='#' method='get'></form>
+     <br></br>
+     <h3>Qual o seu nome?</h3>
+     <form name='f_cadastro' action='#' method='get'></form>
+     <br></br>
+     <br></br>
+     <h3>Qual sua idade?</h3>
+     <form name='f_cadastro' action='#' method='get'></form>
+     <br></br>
+     <br></br>
+     <h3>Qual seu email?</h3>
+     <form name='f_cadastro' action='#' method='get'></form>
+     <br></br>
+     <br></br>
+     <h3>Qual a sua escolaridade?</h3>
+     <form name='f_cadastro' action='#' method='get'>
+     <select name='f_escolaridade'>
+     <option>Ensino fundamental incompleto</option>
+     <option>Ensino fundamental completo</option>
+     <option>Ensino medio incompleto</option>
+     <option>Ensino medio completo</option>
+     <option>Ensino superior incompleto</option>
+     <option>Ensino superior completo</option>
+     </select>
+     <input class='espaco_t quebra' type='subimit' name='f_enviar' value='Enviar'></input>
+     </form>
+    </div>
+  );
+}
+
+export default Etapa1;
---
+++ modulo3/rend-condicional-form/condicional-form/src/Final.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Final() {
+  return (
+    <div className="Final">
+     
+    </div>
+  );
+}
+
+export default Final;
