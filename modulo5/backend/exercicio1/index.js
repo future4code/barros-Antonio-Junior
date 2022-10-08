b2c2c0edd106938d526b4f7d8200099fd7dba4d9
Antonio Junior <antoniojunior_95@hotmail.com>
2022-10-07 22:24:36 -0300

    exercicio-de-revisao-feito-1-2-3

--- modulo5/backend/exercicio1/index.js
+++ modulo5/backend/exercicio1/index.js
@@ -1,1 +1,15 @@
-console.log("exercicio 4");
+function FahrenheitEmKelvin() {
+    let temperaturaDigitada = document.getElementById('temperatura');
+    let temperaturaDigitada = temperaturaDigitada.value;
+    var res = document.getElementById('resultado');
+
+    if (temperaturaDigitada == '') {
+        resultado.innerHTML = '<p style="color: black; font-size: 25px; font-weight: 600;">Digite um número para fazer a conversão.</p>';
+    } else {
+        temperaturaK = (temperaturaDigitada - 32) * 5 / 9 + 273;
+        res.innerHTML = "<p><b>Resultado:</b><br>" + temperaturaK.toFixed(1) + "° Kelvin equivale a " + temperaturaDigitada + "° Fahrenheit.</p>";
+    }
+}
+
+
+console.log("exercicio 1");
--- modulo5/backend/exercicio3/index.js
+++ modulo5/backend/exercicio3/index.js
@@ -1,1 +1,33 @@
+const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  
+
+  const login = () => {
+    firebase.auth().signInWithEmailAndPassword(email, pass).then(user => {
+      console.log(user)
+		})
+  }
+  
+  const cadastro = () => {
+    firebase.auth().createUserWithEmailAndPassword(email, pass).then(user => {
+      console.log(user)
+    })
+  }
+
+
+  return (
+   <Container> 
+     <Title>Seja bem vindo, faça login para continuar </Title>
+      <Input type="email" placeholder="Informe seu email"
+      value={email} onChange={e=> setEmail(e.target.value)}
+      />
+      <Input type="password" placeholder="Informe sua senha"
+      value={pass} onChange={e=> setPass(e.target.value)}
+      />
+      <Button onClick={login}> Entrar com e-mail agora </Button>
+      <Button primary onClick={cadastro}> Cadastre com e-mail agora </Button>
+   </Container>   
+  );
+
+
 console.log("exercicio 3");
